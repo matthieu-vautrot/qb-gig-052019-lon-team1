90d527b522b96444bc0d1e0a24047a3c7a523d50
matthieu-vautrot <matthieu.vautrot@quantumblack.com>
2019-05-09 17:12:15 +0200

    removing by default non intresting features

--- src/R/shinytree/master_col.r
+++ src/R/shinytree/master_col.r
@@ -18,8 +18,7 @@ full_fea_col <- list('unique_participant_id', 'case_type', 'field_office', 'uniq
 
 fea_col <- list('case_type', 'field_office',
 'gender', 'birth_year', 'state_of_current_location', 'state_of_origin',
-'victim_relief_date', 'aftercare_initiated_date',
-'aftercare_closed_date', 'avg_economic_empowerment_score_intake',
+'avg_economic_empowerment_score_intake',
 'avg_health_score_intake', 'avg_housing_score_intake',
 'avg_mental_wellbeing_score_intake', 'avg_protection_score_intake',
 'avg_social_support_score_intake',
@@ -30,7 +29,7 @@ fea_col <- list('case_type', 'field_office',
 'economic_empowerment_and_education_events_count',
 'health_events_count', 'housing_events_count',
 'mental_wellbeing_events_count', 'protection_events_count',
-'support_system_events_count', 'journey_cluster')
+'support_system_events_count')
 
 tg_cols <- list('closed_within_target', 'time_to_closure', 'low_aso',
 'successful_closure', 'dropout')
