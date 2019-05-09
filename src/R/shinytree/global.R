7c43df94a62992903b926fb683afc9f9e34954bb
matthieu-vautrot <matthieu.vautrot@quantumblack.com>
2019-05-09 17:21:50 +0200

    local path fixed

--- src/R/shinytree/global.R
+++ src/R/shinytree/global.R
@@ -1,6 +1,7 @@
 
+# To fix by pointing to master table
 data_src = "/Users/matthew_aaron_cooke/qb-gig-052019-lon-team1/data/05_model_input/model_input.csv"
-
+data_src = "/Users/matthieu_vautrot/work/gig-2019-local/qb-gig-052019-lon-team1/src/R/shinytree/data/master.csv"
 
 source("master_col.r")
 io_path = "/home/matthieu_v/tree_out/"
